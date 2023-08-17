71ba974338697e9bd6be230324f6c4571bae2627
xqzou <zoe.zou@astrazeneca.com>
2023-08-17 13:25:38 +0100

    add GRanges

--- R/POPCORN-package.R
+++ R/POPCORN-package.R
@@ -6,7 +6,7 @@ utils::globalVariables(c("telomere", "centromere","gap","sg","srep"))
 #' @importFrom dplyr mutate filter group_by ungroup summarise select left_join n
 #' @importFrom tidyr replace_na
 #' @importFrom stats median
-#' @importFrom GenomicRanges findOverlaps pintersect width
+#' @importFrom GenomicRanges GRanges findOverlaps pintersect width
 #' @importFrom utils write.table
 ## usethis namespace: end
 NULL
